4f29e9cd20b2d0e8bef886b0476958d99c51fab4
SUA GARCIA JHOAN SEBASTIAN <sebastiansua90@gmail.com>
2025-02-07 01:18:44 -0500

    Terminal x, y format

--- src/main/java/com/udistrital/graphical_method/controller/GraficalMethodController.java
+++ src/main/java/com/udistrital/graphical_method/controller/GraficalMethodController.java
@@ -37,52 +37,65 @@ public class GraficalMethodController {
     @Autowired
     private TwoPhasesService twoPhasesService;
 
-   @PostMapping("/linear-problem")
-public ResponseEntity<?> linearProblem(@RequestBody LinearProblem linearProblem) {
-    try {
-        System.out.println("Received JSON: " + linearProblem.toString());
-        ObjectiveFunction objectiveFunction = linearProblemService
-                .parseObjectiveFunction(linearProblem.getObjectiveFunctionText());
-        List<Restriction> restrictions = linearProblemService
-                .parseRestrictions(linearProblem.getRestrictionsText());
-
-        List<Map<String, Double>> allIntersections = linearProblemService.calculateAllIntersections(restrictions);
-
-        List<Map<String, Double>> refinedIntersections = linearProblemService.refineIntersections(allIntersections,
-                restrictions);
-
-        Map<String, Object> maxResult = linearProblemService.getMax(objectiveFunction, refinedIntersections);
-        Map<String, Object> minResult = linearProblemService.getMin(objectiveFunction, refinedIntersections);
-
-        Double maxValue = (Double) maxResult.get("value");
-        int maxIndex = (int) maxResult.get("index");
-
-        Double minValue = (Double) minResult.get("value");
-        int minIndex = (int) minResult.get("index");
-
-        // Reemplazar las claves x_1 por x y x_2 por y en allIntersections
-        List<Map<String, Double>> updatedIntersections = new ArrayList<>();
-        for (Map<String, Double> intersection : allIntersections) {
-            Map<String, Double> updatedIntersection = new HashMap<>();
-            for (Map.Entry<String, Double> entry : intersection.entrySet()) {
-                String newKey = entry.getKey().replace("x_1", "x").replace("x_2", "y");
-                updatedIntersection.put(newKey, entry.getValue());
+    @PostMapping("/linear-problem")
+    public ResponseEntity<?> linearProblem(@RequestBody LinearProblem linearProblem) {
+        try {
+            System.out.println("Received JSON: " + linearProblem.toString());
+    
+            // Reemplazar las claves x por x_1 y y por x_2 en las restricciones
+            List<String> updatedRestrictionsText = new ArrayList<>();
+            for (String restriction : linearProblem.getRestrictionsText()) {
+                String updatedRestriction = restriction.replace("x", "x_1").replace("y", "x_2");
+                updatedRestrictionsText.add(updatedRestriction);
             }
-            updatedIntersections.add(updatedIntersection);
+            linearProblem.setRestrictionsText(updatedRestrictionsText);
+    
+            // Reemplazar las claves x por x_1 y y por x_2 en la función objetivo
+            String updatedObjectiveFunctionText = linearProblem.getObjectiveFunctionText().replace("x", "x_1").replace("y", "x_2");
+            linearProblem.setObjectiveFunctionText(updatedObjectiveFunctionText);
+    
+            ObjectiveFunction objectiveFunction = linearProblemService
+                    .parseObjectiveFunction(linearProblem.getObjectiveFunctionText());
+            List<Restriction> restrictions = linearProblemService
+                    .parseRestrictions(linearProblem.getRestrictionsText());
+    
+            List<Map<String, Double>> allIntersections = linearProblemService.calculateAllIntersections(restrictions);
+    
+            List<Map<String, Double>> refinedIntersections = linearProblemService.refineIntersections(allIntersections,
+                    restrictions);
+    
+            Map<String, Object> maxResult = linearProblemService.getMax(objectiveFunction, refinedIntersections);
+            Map<String, Object> minResult = linearProblemService.getMin(objectiveFunction, refinedIntersections);
+    
+            Double maxValue = (Double) maxResult.get("value");
+            int maxIndex = (int) maxResult.get("index");
+    
+            Double minValue = (Double) minResult.get("value");
+            int minIndex = (int) minResult.get("index");
+    
+            // Reemplazar las claves x_1 por x y x_2 por y en allIntersections
+            List<Map<String, Double>> updatedIntersections = new ArrayList<>();
+            for (Map<String, Double> intersection : allIntersections) {
+                Map<String, Double> updatedIntersection = new HashMap<>();
+                for (Map.Entry<String, Double> entry : intersection.entrySet()) {
+                    String newKey = entry.getKey().replace("x_1", "x").replace("x_2", "y");
+                    updatedIntersection.put(newKey, entry.getValue());
+                }
+                updatedIntersections.add(updatedIntersection);
+            }
+    
+            LinearProblemResponse response = new LinearProblemResponse();
+            response.setAllIntersections(updatedIntersections);
+            response.setMaxValue(maxValue);
+            response.setMaxIndex(maxIndex);
+            response.setMinValue(minValue);
+            response.setMinIndex(minIndex);
+    
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-
-        LinearProblemResponse response = new LinearProblemResponse();
-        response.setAllIntersections(updatedIntersections);
-        response.setMaxValue(maxValue);
-        response.setMaxIndex(maxIndex);
-        response.setMinValue(minValue);
-        response.setMinIndex(minIndex);
-
-        return ResponseEntity.ok(response);
-    } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
-}
 
     @PostMapping("/two-phases")
     public ResponseEntity<?> twoPhases(@RequestBody LinearProblem linearProblem) {
